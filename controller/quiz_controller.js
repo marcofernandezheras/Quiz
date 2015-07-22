76d8274264fefb15d91287bcdddb382bae964d18
marco <marcofernandezheras@gmail.com>
2015-07-22 20:05:42 +0200

    Layout

--- controller/quiz_controller.js
+++ controller/quiz_controller.js
@@ -1,11 +1,11 @@
 exports.question = function(req, res){
-    res.render('quizes/question', { pregunta : 'Capital de Italia'});
+    res.render('quizes/question', { pregunta : 'Capital de Italia', title: 'Quiz'});
 }
 
 exports.answer = function(req,res){
     if(req.query.respuesta === 'Roma') {
-        res.render('quizes/answer', {respuesta: 'Correcto'});
+        res.render('quizes/answer', {respuesta: 'Correcto', title: 'Quiz'});
     } else {
-        res.render('quizes/answer', {respuesta: 'incorrecto'});
+        res.render('quizes/answer', {respuesta: 'incorrecto', title: 'Quiz'});
     }
 }
