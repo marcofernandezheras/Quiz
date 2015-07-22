cd61b79d16cb411a8551ae3397e72ea4de3d3639
marco <marcofernandezheras@gmail.com>
2015-07-22 20:32:33 +0200

    Css adaptable

--- controller/quiz_controller.js
+++ controller/quiz_controller.js
@@ -1,11 +1,11 @@
 exports.question = function(req, res){
-    res.render('quizes/question', { pregunta : 'Capital de Italia', title: 'Quiz'});
+    res.render('quizes/question', { pregunta : 'Capital de Italia'});
 }
 
 exports.answer = function(req,res){
     if(req.query.respuesta === 'Roma') {
-        res.render('quizes/answer', {respuesta: 'Correcto', title: 'Quiz'});
+        res.render('quizes/answer', {respuesta: 'Correcto'});
     } else {
-        res.render('quizes/answer', {respuesta: 'incorrecto', title: 'Quiz'});
+        res.render('quizes/answer', {respuesta: 'incorrecto'});
     }
 }
